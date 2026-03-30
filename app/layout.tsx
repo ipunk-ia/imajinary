93ebb33ddbe5541a85c1475439be22f06387fed0
ipunk-ia <manunggaltehnik48@gmail.com>
2026-03-30 17:58:29 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css'; // Global styles
 
 export const metadata: Metadata = {
-  title: 'Imajinary Coffee & Space Semarang - Kopi, Dim Sum, Live Music',
+  title: 'Imaginary Coffee & Space Semarang - Kopi, Dim Sum, Live Music',
   description: 'Cafe cozy di Semarang dengan menu kopi specialty, dimsum, dan live music. Tempat nongkrong asik di Kota Semarang.',
 };
 
